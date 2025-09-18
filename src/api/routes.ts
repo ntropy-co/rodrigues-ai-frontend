e2912cc9c7a706a7d1d28884fb1483cc4269ae47
Pablo Bispo <pablofernando@live.com>
2025-09-17 22:33:28 -0300

    Updating api endpoinst

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,23 +1,23 @@
 export const APIRoutes = {
   GetPlaygroundAgents: (PlaygroundApiUrl: string) =>
-    `${PlaygroundApiUrl}/v1/playground/agents`,
+    `${PlaygroundApiUrl}/api/v1/playground/agents`,
   AgentRun: (PlaygroundApiUrl: string) =>
-    `${PlaygroundApiUrl}/v1/playground/agents/{agent_id}/runs`,
+    `${PlaygroundApiUrl}/api/v1/playground/agents/{agent_id}/runs`,
   PlaygroundStatus: (PlaygroundApiUrl: string) =>
-    `${PlaygroundApiUrl}/v1/playground/status`,
+    `${PlaygroundApiUrl}/api/v1/playground/status`,
   GetPlaygroundSessions: (PlaygroundApiUrl: string, agentId: string) =>
-    `${PlaygroundApiUrl}/v1/playground/agents/${agentId}/sessions`,
+    `${PlaygroundApiUrl}/api/v1/playground/agents/${agentId}/sessions`,
   GetPlaygroundSession: (
     PlaygroundApiUrl: string,
     agentId: string,
     sessionId: string
   ) =>
-    `${PlaygroundApiUrl}/v1/playground/agents/${agentId}/sessions/${sessionId}`,
+    `${PlaygroundApiUrl}/api/v1/playground/agents/${agentId}/sessions/${sessionId}`,
 
   DeletePlaygroundSession: (
     PlaygroundApiUrl: string,
     agentId: string,
     sessionId: string
   ) =>
-    `${PlaygroundApiUrl}/v1/playground/agents/${agentId}/sessions/${sessionId}`
+    `${PlaygroundApiUrl}/api/v1/playground/agents/${agentId}/sessions/${sessionId}`
 }
